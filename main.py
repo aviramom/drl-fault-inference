0ead2de1057322e99718e44ae2615257282909b7
Omer Aviram <aviramom@post.bgu.ac.il>
2025-09-07 18:11:13 +0300

    running the diagnosers on all domain at once(one run)

--- main.py
+++ main.py
@@ -17,23 +17,20 @@ if __name__ == '__main__':
         #           "e6000_FrozenLake.json"
         #           "e7000_Breakout.json"
         #
+        domains = ["e2000_Acrobot.json", "e3000_CartPole.json", "e4000_MountainCar.json",
+                   "e5000_Taxi.json", "e6000_FrozenLake.json"]
+        for domain in domains:
+            args = None
+            if len(sys.argv) != 2:
+                args = [sys.argv[0],domain ]
+            else:
+                args = sys.argv
 
-        args = None
-        if len(sys.argv) != 2:
-            args = [sys.argv[0],"e6000_FrozenLake.json" ]
-        else:
-            args = sys.argv
-
-        # ================== experimental setup ==================
-        render_mode = "rgb_array"       # "human", "rgb_array"
-        debug_print = False            # False, True
-        run_experimental_setup_new(arguments=args, render_mode=render_mode, debug_print=debug_print)
-        print(f'finisehd gracefully')
-        mixer.init()
-        mixer.music.load('alarm.mp3')
-        mixer.music.play()
-        while mixer.music.get_busy():  # wait for music to finish playing
-            time.sleep(1)
+            # ================== experimental setup ==================
+            render_mode = "rgb_array"       # "human", "rgb_array"
+            debug_print = False            # False, True
+            run_experimental_setup_new(arguments=args, render_mode=render_mode, debug_print=debug_print)
+            print(f'finisehd {domain} gracefully')
     except ValueError as e:
         print(f'Value error: {e}')
         mixer.init()
@@ -41,5 +38,9 @@ if __name__ == '__main__':
         mixer.music.play()
         while mixer.music.get_busy():  # wait for music to finish playing
             time.sleep(1)
-
+    mixer.init()
+    mixer.music.load('alarm.mp3')
+    mixer.music.play()
+    while mixer.music.get_busy():  # wait for music to finish playing
+        time.sleep(1)
     print(9)
